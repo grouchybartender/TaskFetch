e24db8c769a3ecafe58a45d40e5809f028388b59
Jonathan Compton <jonathankcompton@gmail.com>
2018-11-28 12:20:08 -0800

    Initial Commit

---
+++ TaskFetch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskFetch
+//
+//  Created by Jonathan Compton on 11/28/18.
+//  Copyright © 2018 Jonathan Compton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
